2950cba6faf7f8c93f16709ff949dc4bc30f569e
Incutti <mateincu@gmail.com>
2023-06-16 16:00:37 -0300

    conectamos la base a java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,5 +1,17 @@
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> tablas = Arrays.asList("Jugador", "Posicion", "Fichaje", "Equipo", "Representante", "Representante_has_Equipo", "Equipo_has_Posicion");
+
+        AccesoBaseDeDatos bdd = new AccesoBaseDeDatos("PoliPases", tablas);
+        try {
+            bdd.conectar("alumno", "alumnoipm");
+
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
     }
 }
