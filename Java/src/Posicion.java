b0a6d38e93182af7554f17f578ac6c48d3d7c9db
Incutti <mateincu@gmail.com>
2023-06-26 09:38:14 -0300

    pasamos posicion a enum

--- Java/src/Posicion.java
+++ Java/src/Posicion.java
@@ -1,16 +1,4 @@
-public class Posicion {
-    private String rol;
-
-    public Posicion(String rol) {
-        this.rol = rol;
-    }
-    public Posicion(){rol = "delantero";}
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
+public enum Posicion {
+    DELANTERO, MEDIOCAMPISTA, DEFENSOR, ARQUERO
 }
+
